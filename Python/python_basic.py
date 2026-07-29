4c5c51b0dcf6df9db26b297a12e4917651749bc7
Aravind-103 <aravind.gaddam103@gmail.com>
2026-07-29 23:10:02 +0530

    Added reverse of string code

--- Python/python_basic.py
+++ Python/python_basic.py
@@ -6,3 +6,8 @@ print("Hello World!:)")
 print("Hi, I'm Aravind")
 print("I am learning Python and Data Science")
 print("Welcome to the world of Data Science and Machine Learning")
+
+# Reverse a string
+text = "Python"
+reversed_text = text[::-1]
+print("Reversed string:", reversed_text)
