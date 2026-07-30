6ee91548a6fb0d381e39e9a57d8de60e813a3915
Aravind-103 <aravind.gaddam103@gmail.com>
2026-07-30 22:23:51 +0530

    Flatten String

--- Python/python_basic.py
+++ Python/python_basic.py
@@ -23,3 +23,10 @@ for char in string:
         char_count[char] = 1
 print("Character count:", char_count)
 print("Character appears often", max(char_count, key=char_count.get))
+
+# String flattening
+nested = [[1, 2], [3, 4], [5, 6]]
+flat = [item for sublist in nested for item in sublist]
+print(flat)
+# Output: [1, 2, 3, 4, 5, 6]
+
